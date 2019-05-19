fcdbca57bc50de4c30241a9dc303fffa842d6d9c
Samu Huovinen <samu.huovinen@hotmail.com>
2019-05-19 15:20:47 +0300

    It works now as I want it to work. I have excluded many unneeded edits.

--- visualization.py
+++ visualization.py
@@ -22,26 +22,33 @@ ax.set_zlabel('Z')
 x_coordinates=np.array([])
 y_coordinates=np.array([])
 z_coordinates=np.array([])
-with open("data.csv") as datafile:
+with open("cave_data.csv") as datafile:
     for line in datafile:
         if line=="\n":
             continue
-        data=line.strip().split(",")
+        data=line.split()
+        
+        
+        print(data)
 
-        # csv format:
-    
-        x=float(data[0])
-        y=float(data[1])
-        z=float(data[2])
+        # Traversed distance, unit probably cm?
+        y=float(data[0])
 
+        # Angle, degrees to radians
+        angle=float(data[1])/180*np.pi
+        
+        # Distance from the ceiling or wall, unit probably cm?
+        distance=float(data[2])
 
+        # As a cross section, y is the direction of the movement, z is vertical and x horizontal
 
-        print(x,y,z)
-        x_coordinates=np.append(x_coordinates,x)
-        y_coordinates=np.append(y_coordinates,y)
-        z_coordinates=np.append(z_coordinates,z)
+        # Use some trigonometry to place points
+        print(angle)
+        x_coordinates=np.append(x_coordinates,distance*np.cos(angle)*0.1)
+        y_coordinates=np.append(y_coordinates,data[0])
+        z_coordinates=np.append(z_coordinates,distance*np.sin(angle)*0.1)
 
-print(x_coordinates,y_coordinates,z_coordinates)
+#print(x_coordinates,y_coordinates,z_coordinates)
 ax.scatter(xs=x_coordinates,ys=y_coordinates,zs=z_coordinates,c="red")
 
 plt.show()
